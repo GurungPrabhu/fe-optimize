8ef0d25f1f0000afeb2c43fcd9050e93adfdfd58
GurungPrabhu <prabhu.eversoft@gmail.com>
2024-03-14 19:00:47 +0545

    feat: trigger both

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
     "eslint:recommended",
-
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
